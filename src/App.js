c11dd13f9b6bbb474e659d004a1c4eb19874a0f8
Mohib Ismail <imohib168@gmail.com>
2020-08-30 14:13:57 +0500

    Added new line into App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App({ name, age }) {
   return (
     <div className="App">Hello My Name is {name} and My Age is {age}
       <p>This is just calculation of 25 - 10 = {25 - 10}</p>
+      <p>This is just calculation of 55 - 10 = {55 - 10}</p>
 
       <Hello dishName1="Karahi" dishName2="Biryani" dishName3="Qourma" dishName4="Haleem"/>
     </div>
